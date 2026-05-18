92a09e2fc61e074a2e50059da2dd33e24edccc23
FawjiyaEifa-gif <23201008@uap-bd.edu>
2026-05-18 22:00:49 +0600

    Added exam model

--- panicplanner/subjects/models.py
+++ panicplanner/subjects/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subject(models.Model):
+      User = models.ForeignKey(User,on_delete=models.CASCADE)
+      name= models.CharField(max_length=200)
+      description= models.TextField( blank= True)
+      exam_date=models.DateField()
+      created_at=models.DateTimeField(auto_now_add=True)
+
+
+def __srt__(self):
+      return self.name
+
+
+
+class Exam(models.Model):
+      subject= models.ForeignKey(Subject,on_delete=models.CASCADE)
+
+      title= models.CharField(max_length=200)
+
+      exam_date=models.DateField()
+      total_marks=models.IntegerField()
+
+      def __str__(self):
+            return self.title
+
+
