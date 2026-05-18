259ae3af2ef50566f20e14a102bef9aa5227754d
Asus <anika.at113@gmail.com>
2026-05-18 14:02:43 +0600

    implemented user signup backend logic

--- panicplanner/accounts/views.py
+++ panicplanner/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def signup(request):
+
+     if request.method == "POST":
+         username = request.POST['username']
+         password = request.POST['password']
+
+         user = User.objects.create_user(
+             username=username,
+             password=password
+         )
+
+         user.save()
+
+         return redirect('login')
+
+     return render(request, 'accounts/signup.html')
