7438ee6eb1a67877417d8b6642e584abcc1b5cbd
Asus <anika.at113@gmail.com>
2026-05-19 00:08:20 +0600

    added user login authentication

--- panicplanner/accounts/views.py
+++ panicplanner/accounts/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+def home(request):
+    return render(request, 'accounts/home.html')
 
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-     if request.method == "POST":
-         username = request.POST['username']
-         password = request.POST['password']
+        User.objects.create_user(username=username, password=password)
+        return redirect('login')
 
-         user = User.objects.create_user(
-             username=username,
-             password=password
-         )
+    return render(request, 'accounts/signup.html')
 
-         user.save()
 
-         return redirect('login')
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-     return render(request, 'accounts/signup.html')
+        user = authenticate(
+            request,
+            username=username,
+            password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'accounts/login.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
