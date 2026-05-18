0fa07f0077eee99337c46c0cfc5bbb6daa86c637
FawjiyaEifa-gif <23201008@uap-bd.edu>
2026-05-18 22:15:33 +0600

    Configured admin panel

--- panicplanner/subjects/admin.py
+++ panicplanner/subjects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Subject
+from .models import Exam
+from .models import Topic
+
+admin.site.register(Subject)
+admin.site.register(Exam)
+admin.site.register(Topic)
+
 
 # Register your models here.
