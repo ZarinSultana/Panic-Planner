ea5bad4ec6499d83c557718c2c8da6d63bc51ef2
ASUS <zarinsultanar@gmail.com>
2026-05-19 01:30:02 +0600

    created planner app structure

--- panicplanner/panicplanner/urls.py
+++ panicplanner/panicplanner/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('subjects/',include('subjects.urls')),
+    path('', include('planner.urls')),
 ]
