a03a876592688bb51595807d0b0d77d7f3618866
Asus <anika.at113@gmail.com>
2026-05-19 15:01:55 +0600

    implemented logout function

--- panicplanner/accounts/views.py
+++ panicplanner/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 def home(request):
     return render(request, 'accounts/home.html')
