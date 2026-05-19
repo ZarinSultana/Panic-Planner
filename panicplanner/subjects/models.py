c10ba8e87564dab4bc15a778b06bce4944a4fde7
FawjiyaEifa-gif <23201008@uap-bd.edu>
2026-05-20 01:38:28 +0600

    update

--- panicplanner/subjects/models.py
+++ panicplanner/subjects/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Subject(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
@@ -11,6 +12,7 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
+
 
 
 class Exam(models.Model):
